a4ea4127f3df14c409b66479731d60ac74f62546
--replace-all <akshar.barot67@gmail.com>
2022-04-28 21:21:39 -0400

    Added 2017 Open P1 The Lose Cow

---
+++ Bronze Division/2017/US Open/P1 The Lost Cow/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader (new FileReader ("lostcow.in"));
+        PrintWriter out = new PrintWriter (new FileWriter ("lostcow.out"));
+        StringTokenizer st = new StringTokenizer (in.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int total = 0;
+        int last = 0;
+
+        for (int i = 0; i < 9000; i ++){
+            int cur = (int) Math.pow (2, i);
+            total += last;
+            total += cur;
+            last = cur;
+            if (i%2 == 0){
+                if (x + cur >= y && y >= x){
+                    total -= (x+cur)-y;
+                    break;
+                }
+            }
+            else {
+                if (x - cur <= y && y <= x){
+                    total -= y-(x-cur);
+                    break;
+                }
+            }
+        }
+        out.println (total);
+        out.close();
+    }
+}
